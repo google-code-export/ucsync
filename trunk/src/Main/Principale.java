b4aced0ad858e551e957caf90e69e2ac83b7e765
alexandre.ratel@gmail.com <alexandre.ratel@gmail.com@37e6e571-0f0f-ea1f-1749-e72027ad4cb2>
2013-06-20 21:35:19 +0000

    First Version just to try subversion

---
+++ trunk/src/Main/Principale.java
@@ -0,0 +1,14 @@
+package Main;
+
+/*********************************************/
+public class Principale
+	{
+
+	/****************************************/
+	public static void main(String[] args)
+		{
+		// TODO Auto-generated method stub
+
+		}
+	/*2013*//*RATEL Alexandre 8)*/
+	}
