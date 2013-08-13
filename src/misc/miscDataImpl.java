ce821074d5722e7344c460cecde42445bf9f770d
alexandre.ratel@gmail.com <alexandre.ratel@gmail.com@37e6e571-0f0f-ea1f-1749-e72027ad4cb2>
2013-08-13 11:14:59 +0000

    scan process writing is finished

---
+++ src/misc/miscDataImpl.java
@@ -0,0 +1,19 @@
+package misc;
+
+/**********************************
+ * 
+ * 
+ * @author RATEL Alexandre
+ **********************************/
+public interface miscDataImpl
+	{
+	
+	/**
+	 * Method used to launch getting data 
+	 * process
+	 */
+	public void autoComplete() throws Exception;
+	
+	/*2013*//*RATEL Alexandre 8)*/
+	}
+
--- src/scan/deviceAssociatedLine.java
+++ src/scan/deviceAssociatedLine.java
@@ -11,14 +11,19 @@ public class deviceAssociatedLine
 	/**
 	 * Variables
 	 */
-	private String devicePkid;
-	private String linePkid;
+	private String devicePkid, linePkid;
+	private String displayName, lineTextLabel, externalPhoneNumberMask;
+	private int index;
 	
 	
-	public deviceAssociatedLine(String devicePkid, String linePkid)
+	public deviceAssociatedLine(String devicePkid, String linePkid, String displayName, String lineTextLabel, String externalPhoneNumberMask, int index)
 		{
 		this.devicePkid = devicePkid;
 		this.linePkid = linePkid;
+		this.displayName = displayName;
+		this.lineTextLabel = lineTextLabel;
+		this.externalPhoneNumberMask = externalPhoneNumberMask;
+		this.index = index;
 		}
 
 	
@@ -41,6 +46,46 @@ public class deviceAssociatedLine
 	public void setLinePkid(String linePkid)
 		{
 		this.linePkid = linePkid;
+		}
+
+	public String getDisplayName()
+		{
+		return displayName;
+		}
+
+	public void setDisplayName(String displayName)
+		{
+		this.displayName = displayName;
+		}
+
+	public String getLineTextLabel()
+		{
+		return lineTextLabel;
+		}
+
+	public void setLineTextLabel(String lineTextLabel)
+		{
+		this.lineTextLabel = lineTextLabel;
+		}
+
+	public String getExternalPhoneNumberMask()
+		{
+		return externalPhoneNumberMask;
+		}
+
+	public void setExternalPhoneNumberMask(String externalPhoneNumberMask)
+		{
+		this.externalPhoneNumberMask = externalPhoneNumberMask;
+		}
+
+	public int getIndex()
+		{
+		return index;
+		}
+
+	public void setIndex(int index)
+		{
+		this.index = index;
 		}
 
 	
