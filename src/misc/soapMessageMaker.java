c67b64c64040c11c383a4f2a537f85ded69764c8
alexandre.ratel@gmail.com <alexandre.ratel@gmail.com@37e6e571-0f0f-ea1f-1749-e72027ad4cb2>
2013-08-19 22:50:09 +0000

    Temporary file, do not use this version

--- src/misc/soapMessageMaker.java
+++ src/misc/soapMessageMaker.java
@@ -12,6 +12,8 @@ import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPFactory;
 import javax.xml.soap.SOAPMessage;
 
+import scan.device;
+import scan.line;
 import schedule.userSync;
 import schedule.userSync.patternType;
 import utils.methodesUtiles;
@@ -25,7 +27,7 @@ import utils.methodesUtiles;
 public class soapMessageMaker
 	{
 	
-	public static SOAPMessage make(patternType type, String newData, userSync myUSync, String UUID) throws Exception
+	public static SOAPMessage make(patternType type, String newData, userSync myUSync, device d) throws Exception
 		{
 		String cmdName;
 		SOAPMessage soapMessage = null;
@@ -33,29 +35,69 @@ public class soapMessageMaker
 		if(type.equals(patternType.devicedescription))
 			{
 			cmdName = "updatePhone";
-		    SOAPElement mySOAPEleExt = getPreparedHeader(soapMessage, cmdName, methodesUtiles.getTargetTask("axlport", myUSync.getTaskIndex()), UUID);
+		    SOAPElement mySOAPEleExt = getPreparedHeader(soapMessage, cmdName, methodesUtiles.getTargetTask("axlport", myUSync.getTaskIndex()), d.getUUID());
 			
 		    mySOAPEleExt.addChildElement("description").addTextNode(newData);
 			}
-		else if(type.equals(patternType.linealertingname))
+		
+		return soapMessage;
+		}
+	
+	public static SOAPMessage make(patternType type, String newData, userSync myUSync, line l) throws Exception
+		{
+		String cmdName;
+		SOAPMessage soapMessage = null;
+		SOAPEnvelope envelope = soapMessage.getSOAPPart().getEnvelope();
+		
+		if(type.equals(patternType.linealertingname))
 			{
+			cmdName = "updateLine";
+		    SOAPElement mySOAPEleExt = getPreparedHeader(soapMessage, cmdName, methodesUtiles.getTargetTask("axlport", myUSync.getTaskIndex()), l.getUUID());
 			
+		    mySOAPEleExt.addChildElement("alertingName").addTextNode(newData);
+		    mySOAPEleExt.addChildElement("asciiAlertingName").addTextNode(newData);
 			}
 		else if(type.equals(patternType.linedescription))
 			{
+			cmdName = "updateLine";
+		    SOAPElement mySOAPEleExt = getPreparedHeader(soapMessage, cmdName, methodesUtiles.getTargetTask("axlport", myUSync.getTaskIndex()), l.getUUID());
 			
+		    mySOAPEleExt.addChildElement("description").addTextNode(newData);
 			}
 		else if(type.equals(patternType.linedisplay))
 			{
+			cmdName = "updatePhone";
+		    SOAPElement mySOAPEleExt = getPreparedHeader(soapMessage, cmdName, methodesUtiles.getTargetTask("axlport", myUSync.getTaskIndex()), l.getPhoneUUID());
 			
+		    SOAPElement mySOAPEleExte = mySOAPEleExt.addChildElement("lines");
+		    SOAPElement mySOAPEleExten = mySOAPEleExte.addChildElement("line");
+		    mySOAPEleExten.addChildElement("dirn").addAttribute(envelope.createName("uuid"), l.getUUID());
+		    mySOAPEleExten.addChildElement("index").addTextNode(Integer.toString(l.getIndex()));
+		    mySOAPEleExten.addChildElement("display").addTextNode(newData);
+		    mySOAPEleExten.addChildElement("displayAscii").addTextNode(newData);
 			}
 		else if(type.equals(patternType.linetextlabel))
 			{
+			cmdName = "updatePhone";
+		    SOAPElement mySOAPEleExt = getPreparedHeader(soapMessage, cmdName, methodesUtiles.getTargetTask("axlport", myUSync.getTaskIndex()), l.getPhoneUUID());
 			
+		    SOAPElement mySOAPEleExte = mySOAPEleExt.addChildElement("lines");
+		    SOAPElement mySOAPEleExten = mySOAPEleExte.addChildElement("line");
+		    mySOAPEleExten.addChildElement("dirn").addAttribute(envelope.createName("uuid"), l.getUUID());
+		    mySOAPEleExten.addChildElement("index").addTextNode(Integer.toString(l.getIndex()));
+		    mySOAPEleExten.addChildElement("label").addTextNode(newData);
+		    mySOAPEleExten.addChildElement("asciiLabel").addTextNode(newData);
 			}
 		else if(type.equals(patternType.lineexternalphonenumbermask))
 			{
+			cmdName = "updatePhone";
+		    SOAPElement mySOAPEleExt = getPreparedHeader(soapMessage, cmdName, methodesUtiles.getTargetTask("axlport", myUSync.getTaskIndex()), l.getPhoneUUID());
 			
+		    SOAPElement mySOAPEleExte = mySOAPEleExt.addChildElement("lines");
+		    SOAPElement mySOAPEleExten = mySOAPEleExte.addChildElement("line");
+		    mySOAPEleExten.addChildElement("dirn").addAttribute(envelope.createName("uuid"), l.getUUID());
+		    mySOAPEleExten.addChildElement("index").addTextNode(Integer.toString(l.getIndex()));
+		    mySOAPEleExten.addChildElement("e164Mask").addTextNode(newData);
 			}
 		
 		return soapMessage;
